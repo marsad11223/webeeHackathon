272f6ea4556398071b601e3532cb075cfb857037
marsad11223 <marsad11223@gmail.com>
2023-07-28 16:33:00 +0500

    Setup Navigation

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import { store } from './src/store/store';
+import Navigator from './src/navigation/Navigator';
 
 function App(): JSX.Element {
-  const Stack = createNativeStackNavigator();
-  const Drawer = createDrawerNavigator();
 
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <SafeAreaView style={styles.container}>
-          <NavigationContainer>
-            <Drawer.Navigator initialRouteName="Home">
-              <Drawer.Screen name="Home" component={HomeScreen} />
-            </Drawer.Navigator>
-          </NavigationContainer>
+          <Navigator />
         </SafeAreaView>
       </GestureHandlerRootView>
     </Provider>
---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import HomeScreen from '../screens/HomeScreen';
+
+function Navigator() {
+  const Drawer = createDrawerNavigator();
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigator;
