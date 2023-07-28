e593fe256bf6492416e4c0a0ca14ae7babfe37d6
marsad11223 <marsad11223@gmail.com>
2023-07-28 16:49:45 +0500

    Implement Add Item screen

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Dashboard from '../screens/Dashboard';
 import Categories from '../screens/Categories';
+import Items from '../screens/Items';
+import { useAppSelector } from '../store/hooks';
 
 function Navigator() {
   const Drawer = createDrawerNavigator();
+  const { categories } = useAppSelector(state => state.category)
 
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Dashboard">
         <Drawer.Screen name="Dashboard" component={Dashboard} />
         <Drawer.Screen name="Categories" component={Categories} />
+        {categories.map((Catageory) => {
+          return (
+            <Drawer.Screen name={Catageory.name || 'UNNAME CATEGEORY'} component={Items} initialParams={{ category: Catageory }} />
+          )
+        })}
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Items.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Items: React.FC = ({ route }) => {
+
+  console.log(route.params);
+
+  return (
+    <View >
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default Items;
